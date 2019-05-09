8374341c1057c72783e674dcf7fc57fa4e4cb9f8
trapa <1982gaya@gmail.com>
2019-05-10 00:10:38 +0900

    edit&add component element

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,18 @@ import './Movie.css';
 class Movie extends Component {
     render() {
         return (
-            <h1>hello</h1>
+            <div className="right">
+                <div className="headers">
+                    <div className="input-area">
+                    <input type="text" placeholder="Search movie" />
+                    </div>
+                    <select>
+                    <option value="Trending" className="opt">Trending</option>
+                    <option value="Latest" className="opt">Latest</option>
+                    </select>
+                </div>
+                <div className="movie"></div>
+            </div>
         );
     }
 }
