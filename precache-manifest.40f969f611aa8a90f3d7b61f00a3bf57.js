a5382a35d915424f2ceed77aff5e52be0eed9f96
trapa <1982gaya@gmail.com>
2019-05-11 22:16:16 +0900

    Updates

---
+++ precache-manifest.40f969f611aa8a90f3d7b61f00a3bf57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebe516daadb2cf39718799b6addebba",
+    "url": "/movie-dashboard/index.html"
+  },
+  {
+    "revision": "5e1d0c7f381b9225c3cf",
+    "url": "/movie-dashboard/static/css/main.6b62653b.chunk.css"
+  },
+  {
+    "revision": "5ac83c61300d737c1ffb",
+    "url": "/movie-dashboard/static/js/2.29c16ea6.chunk.js"
+  },
+  {
+    "revision": "5e1d0c7f381b9225c3cf",
+    "url": "/movie-dashboard/static/js/main.5b58f290.chunk.js"
+  },
+  {
+    "revision": "21833e505e692ebb54dc",
+    "url": "/movie-dashboard/static/js/runtime~main.ffc80106.js"
+  }
+]);
